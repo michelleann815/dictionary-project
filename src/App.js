7eef8609d8118f238cdd2d134c5b6936f6fd7494
michelleann815 <68318965+michelleann815@users.noreply.github.com>
2021-06-30 14:23:54 +0900

    created new component Dictionary.js, set up form with input type "search" with functions to search for "keyword"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       </header>
-      Hello
+      <main><Dictionary /></main>
+      <footer className="signature">
+        <small>Coded by Michelle Terrazas</small>
+      </footer>
+      </div>
     </div>
   );
 }
