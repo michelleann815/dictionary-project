f871af92fbc8715db78073cd0446f7c8a28a170c
michelleann815 <68318965+michelleann815@users.noreply.github.com>
2021-07-11 21:12:26 +0900

    Integrated Photos, added some changes to styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -16,7 +22,13 @@ export default function Dictionary() {
 
     //documentation https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "563492ad6f91700001000001e67f9d1fe1914ad9b8119960692e567e";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -39,6 +51,7 @@ export default function Dictionary() {
         </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
---
+++ src/Photos.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="container">
+          <div className="row">
+            {props.photos.map(function (photo, index) {
+              return (
+                <div className="col-4" key={index}>
+                  <a href={photo.src.original} target="_blank" rel="noreferrer">
+                    <img src={photo.src.landscape} alt=" " />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
