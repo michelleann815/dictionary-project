13bc1a93dbc288f4701fe2ce3d7243f3a644d6eb
michelleann815 <68318965+michelleann815@users.noreply.github.com>
2021-07-11 19:06:40 +0900

    Added background image to entire app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
+import "./App.css";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-      </header>
-      <main><Dictionary /></main>
-      <footer className="signature">
-        <small>Coded by Michelle Terrazas</small>
-      </footer>
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="signature">
+          <small>Coded by Michelle Terrazas</small>
+        </footer>
       </div>
     </div>
   );
